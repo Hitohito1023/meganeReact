7b58a89f350b152209b991bb8f837a3f4c3bb467
tanno.masayoshi <tanno.masayoshi@m.alhinc.jp>
2021-08-04 17:54:29 +0900

    route-dom関連

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Top from './components/Top';
-import Login from './components/Login';
+
+import top from './components/main/top';
+import login from './components/user/login';
+import edit from './components/user/editUser';
+import users from './components/user/userList';
+import select from './components/user/selectUser';
+import signup from './components/user/signup';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Top} />
-        <Route path='/login' component={Login} />
+        <Route exact path='/' component={top} />
+        <Route path='/login' component={login} />
         <Route path='/users/edit' component={edit} />
         <Route path='/users' component={users} />
         <Route path='/users/select' component={select} />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import reducer from './reducer/reducer';
+import { createStore } from 'redux';
+import { CookiesProvider } from 'react-cookie';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <CookiesProvider>
